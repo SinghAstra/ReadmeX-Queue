dacc41605bda2230c61b261e898f4ff6dff1d729
Astra <abhaypratapsinghwd@gmail.com>
2024-04-06 14:23:06 +0530

    Added Flask API for fetching stock data from Google Finance, with error handling and response formatting.

--- server.py
+++ server.py
@@ -3,17 +3,25 @@ from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
-print("__name__ is ",__name__)
 
-def fetch_stock_price(ticker, exchange):
+def fetch_stock_data(ticker, exchange):
     url = f"https://www.google.com/finance/quote/{ticker}:{exchange}"
     headers = {}
     try:
         search_response = requests.get(url, headers=headers)
         if search_response.status_code == 200:
             soup = BeautifulSoup(search_response.content, 'html.parser')
-            nse_price = float(soup.find('div', {'class': "YMlKec fxKbKc"}).get_text().replace("₹", "").replace(",", ""))
-            return nse_price
+            data = {}
+            stock_price  = soup.find("div",{"class":"YMlKec fxKbKc"}).get_text()
+            data["Stock Price"] = stock_price
+            dataDivs = soup.find_all("div", {"class": "gyFHrc"})
+            for dataDiv in dataDivs:
+                key = dataDiv.find("span").find("div", {"class": "mfs7Fc"})
+                if key is not None:
+                    key = key.get_text()
+                    val = dataDiv.findChild("div" ,{"class":"P6K39c"} ).get_text()
+                    data[key] = val
+            return data
         else:
             return None
     except Exception as e:
@@ -21,14 +29,14 @@ def fetch_stock_price(ticker, exchange):
         return None
 
 @app.route('/stock_price')
-def get_stock_price():
+def get_stock_info():
     ticker = request.args.get('ticker')
     exchange = request.args.get('exchange')
     if not ticker or not exchange:
         return jsonify({'error': 'Ticker and exchange are required'}), 400
-    price = fetch_stock_price(ticker, exchange)
-    if price is not None:
-        return jsonify({'price': price})
+    data = fetch_stock_data(ticker, exchange)
+    if data is not None:
+        return jsonify(data)
     else:
         return jsonify({'error': 'Failed to fetch data'})
 
