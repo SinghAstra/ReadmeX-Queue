41a2d9fa340f145058a96158b795dbdaa69dd5ff
Astra <abhaypratapsinghwd@gmail.com>
2024-12-31 04:33:54 +0530

    Initial Project Setup

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "BlogX",
+  description: "A modern platform to share your thoughts, stories, and ideas.",
+  tagline: "Let's Learn Together",
+  headline: "Share Ideas, Inspire Minds",
+  subHeadline: "Empowering everyone to tell their stories.",
+  url: "https://blogx.vercel.app",
+  features: [
+    "Write and publish articles seamlessly",
+    "Engage with a community of readers and writers",
+    "Explore trending topics and curated content",
+  ],
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra/blogx",
+    linkedIn: "https://www.linkedin.com/in/singhastra",
+  },
+};
