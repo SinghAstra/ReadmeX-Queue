335420870eec460d647cf4ba30f2fab8e6e42de6
Astra <abhaypratapsinghwd@gmail.com>
2025-04-05 17:23:30 +0530

    Updated Hero Section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,10 +85,19 @@ const config: Config = {
             height: "0",
           },
         },
+        shine: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-400% 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shine: "shine 6s linear infinite",
       },
     },
   },
